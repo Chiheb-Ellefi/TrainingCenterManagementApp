84db04854addeb5b9cd30e99f33a51ee86271e08
chihebellefi <chihebellefi888@gmail.com>
2025-04-11 05:30:17 +0100

    update

---
+++ src/main/java/com/example/CenterManagement/services/users/UserService.java
@@ -0,0 +1,55 @@
+package com.example.CenterManagement.services.users;
+
+import com.example.CenterManagement.dto.user.UserDto;
+import com.example.CenterManagement.exceptions.users.UserNotFoundException;
+import com.example.CenterManagement.mappers.user.UserMapper;
+import com.example.CenterManagement.repositories.users.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@Transactional
+public class UserService {
+    private final UserRepository userRepository;
+    @Value("${spring.application.offset}")
+     private int offset ;
+    @Autowired
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public List<UserDto> getAllUsers(int page) {
+        return userRepository.findAll(PageRequest.of(page,offset ))
+                .stream()
+                .map(UserMapper::toDto)
+                .collect(Collectors.toList());
+    }
+
+    public UserDto getUserById(Long id) {
+        return userRepository.findById(id)
+                .map(UserMapper::toDto)
+                .orElseThrow(() -> new UserNotFoundException("User not found with ID: " + id));
+    }
+    @Transactional
+    public UserDto createUser(UserDto userDto) {
+        return UserMapper.toDto(userRepository.save(UserMapper.toEntity(userDto)));
+    }
+
+    public UserDto updateUser(UserDto userDto) {
+        return UserMapper.toDto(userRepository.save(UserMapper.toEntity(userDto)));
+    }
+
+    public void deleteUser(Long id) {
+        if ( !userRepository.existsById(id)) {
+            throw new UserNotFoundException("User not found with ID: " + id);
+        }
+        userRepository.deleteById(id);
+
+    }
+}
